2167366b2941f9d98611a79cba9de18c9fbf4a74
yuvrajshrirame <mail.yuvrajshrirame@gmail.com>
2025-01-10 20:33:43 +0530

    Updated utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -12,4 +12,6 @@ export const handleError = (msg) => {
     })
 }
 
-export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+// export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
+export const APIUrl = process.env.REACT_APP_API_URL || 'https://finfusion-backend.onrender.com';
